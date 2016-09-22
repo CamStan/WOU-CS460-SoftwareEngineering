13cbcf8ef8192354b2b7f105a60e45dc8443c2d7
CStanavige <cstanavige12@wou.edu>
2016-09-22 15:05:41 -0700

    Bootstrap organization files and index file

---
+++ HW1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Bootstrap Example</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+<!--        Stylesheets-->
+        <link rel="stylesheet" href="styles/css/bootstrap.min.css">
+        
+<!--        Scripts-->
+        <script src="styles/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        
+        <div class="container">
+            <h1>My First Bootstrap Page</h1>
+            <p>Hello World!</p>
+        </div>
+        
+    </body>
+</html>
