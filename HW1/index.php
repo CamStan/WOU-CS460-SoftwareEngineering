ead9dfa09b1239ec6407ddb43b08ce1e096b427f
CStanavige <cstanavige12@wou.edu>
2016-09-30 23:50:27 -0700

    Created and styled a footer

--- HW1/index.php
+++ HW1/index.php
@@ -2,9 +2,4 @@
 
 <center><h2>Contents of each page will go here</h2></center>
 
-    </div>
-    <!-- end container div -->
-
-    </body>
-
-    </html>
+ <?php include ('layouts/footer.php'); ?>
---
+++ HW1/layouts/footer.php
@@ -0,0 +1,35 @@
+<div id="footer" class="row">
+    <div class="container-fluid vcenter">
+        <hr />
+        <ul>
+            <li>
+                <a title="Follow me on Facebook" href="#"><img class="icon img-rounded" src="images/FacebookIcon.png" /></a>
+            </li>
+            <li>
+                <a title="Follow me on Twitter" href="#"><img class="icon img-rounded" src="images/TwitterIcon.png" /></a>
+            </li>
+            <li>
+                <a title="Follow me on Google+" href="#"><img class="icon img-rounded" src="images/GooglePlusIcon.png" /></a>
+            </li>
+            <li>
+                <a title="Connect with me on Linkedin" href="#" target="_blank"><img class="icon img-rounded" src="images/LinkedinIcon.png" /></a>
+            </li>
+            <li>
+                <a title="Check out my Bitbucket" href="#"><img class="icon img-rounded" src="images/BitbucketIcon.png" /></a>
+            </li>
+            <li>
+                <a title="Check out my Github" href="#"><img class="icon img-rounded" src="images/GithubIcon.png" /></a>
+            </li>
+            <li>
+                <a title="Email me" href="#"><img class="icon img-rounded" src="images/GmailIcon.png" /></a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+</div>
+<!-- end container div -->
+
+</body>
+
+</html>
