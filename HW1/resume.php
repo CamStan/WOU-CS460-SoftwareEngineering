6661274abb082cab580c5f9d0f09c01f379286a2
CStanavige <cstanavige12@wou.edu>
2016-10-01 02:20:45 -0700

    Began creation of resume page

--- HW1/resume.php
+++ HW1/resume.php
@@ -1,5 +1,100 @@
 <?php include ('layouts/header.php'); ?>
 
-<center><h2>Contents of each page will go here</h2></center>
+    <center>
+        <h1>Resum&eacute;</h1></center>
+    <table class="table table-striped table-bordered" id="table-resume">
+        <tr>
+            <td class="resume-category">
+                <h2>Education</h2>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>
+                <dl>
+                    <dt><h4><strong>Western Oregon University</strong></h4></dt>
+                    <dd>Anticipated Graduation: June 2018</dd>
+                </dl>
+            </td>
+            <td>
+                <ul>
+                    <li>Major: Computer Science & Mathematics</li>
+                    <li>GPA: 4.0</li>
+                    <li>Phi Kappa Phi Inductee, Computer Science Club's Chief Financial Officer</li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <td class="resume-category">
+                <h2>Experience</h2>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>
+                <dl>
+                    <dt><h4><strong>Math Tutor & Grader</strong></h4></dt>
+                    <dd>Western Oregon University</dd>
+                    <dd>September 2016 - Present</dd>
+                </dl>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>
+                <dl>
+                    <dt><h4><strong>Sergeant</strong></h4></dt>
+                    <dd>Oregon Army National Guard</dd>
+                    <dd>April 201 - Present</dd>
+                </dl>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>
+                <dl>
+                    <dt><h4><strong>Assistant Web Developer</strong></h4></dt>
+                    <dd>The Research Institute at</dd>
+                    <dd>Western Oregon University</dd>
+                    <dd>July 2015 - September 2016</dd>
+                </dl>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>
+                <dl>
+                    <dt><h4><strong>PCN Surveyor</strong></h4></dt>
+                    <dd>USDA</dd>
+                    <dd>April 2012 - April 2013</dd>
+                </dl>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>
+                <dl>
+                    <dt><h4><strong>Cannon Crew Member</strong></h4></dt>
+                    <dd>Idaho Army National Guard</dd>
+                    <dd>June 2003 - April 2013</dd>
+                </dl>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td class="resume-category">
+                <h2>Volunteer</h2>
+            </td>
+            <td></td>
+        </tr>
+        <tr></tr>
+        <tr></tr>
+        <tr>
+            <td class="resume-category">
+                <h2>Skills</h2>
+            </td>
+            <td></td>
+        </tr>
+    </table>
 
- <?php include ('layouts/footer.php'); ?>
+    <?php include ('layouts/footer.php'); ?>
