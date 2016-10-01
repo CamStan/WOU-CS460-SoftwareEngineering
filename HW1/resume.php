401d9d516c767f7890440ac2b85cc8cfbb65d03f
CStanavige <cstanavige12@wou.edu>
2016-10-01 14:48:41 -0700

    Finished resume page and styling

--- HW1/resume.php
+++ HW1/resume.php
@@ -38,17 +38,30 @@
                     <dd>September 2016 - Present</dd>
                 </dl>
             </td>
-            <td></td>
+            <td>
+                <ul>
+                    <li>One-on-one and group tutoring of mathematics students</li>
+                    <li>Grading for two Applied Descrete Mathematics courses</li>
+                    <li>Aid in the opeation of walk-in math tutoring cetner on campus</li>
+                </ul>
+            </td>
         </tr>
         <tr>
             <td>
                 <dl>
                     <dt><h4><strong>Sergeant</strong></h4></dt>
                     <dd>Oregon Army National Guard</dd>
-                    <dd>April 201 - Present</dd>
+                    <dd>April 2013 - Present</dd>
                 </dl>
             </td>
-            <td></td>
+            <td>
+                <ul>
+                    <li>Operate and maintain artillary equipment in a high-stress environment</li>
+                    <li>Responsible for $2.5 million of equipment</li>
+                    <li>Training small groups of Soldiers to ensure constant state of rediness</li>
+                    <li>Evaluate Soldiers under command with high level of detail and accuracy</li>
+                </ul>
+            </td>
         </tr>
         <tr>
             <td>
@@ -59,7 +72,13 @@
                     <dd>July 2015 - September 2016</dd>
                 </dl>
             </td>
-            <td></td>
+            <td>
+                <ul>
+                    <li>Built and maintained multiple website pages and elements</li>
+                    <li>Collaborated with team to ensure effient workflow</li>
+                    <li>Entered data with high accuracy</li>
+                </ul>
+            </td>
         </tr>
         <tr>
             <td>
@@ -69,7 +88,13 @@
                     <dd>April 2012 - April 2013</dd>
                 </dl>
             </td>
-            <td></td>
+            <td>
+                <ul>
+                    <li>Conducted field surveys in collaboration with large teams</li>
+                    <li>Inspected samples for elusive Pale Cyst Nematode eggs</li>
+                    <li>Used and maintained sensitive laboratory equipment</li>
+                </ul>
+            </td>
         </tr>
         <tr>
             <td>
@@ -79,21 +104,39 @@
                     <dd>June 2003 - April 2013</dd>
                 </dl>
             </td>
-            <td></td>
-        </tr>
-        <tr>
-            <td class="resume-category">
-                <h2>Volunteer</h2>
+            <td>
+                <ul>
+                    <li>Trained groups of American and Iraqi soldiers</li>
+                    <li>Operated and maintained multiple weapons systems</li>
+                    <li>Operated and maintained several large wheeled and tracked vehicles</li>
+                    <li>Volunteered to fight seasonal forest fires</li>
+                </ul>
             </td>
-            <td></td>
         </tr>
-        <tr></tr>
-        <tr></tr>
         <tr>
             <td class="resume-category">
                 <h2>Skills</h2>
             </td>
-            <td></td>
+            <td id="skills-cell">
+                <ul>
+                    <li>Java</li>
+                    <li>C</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                </ul>
+                <ul>
+                    <li>Javascript</li>
+                    <li>PHP</li>
+                    <li>SQL</li>
+                    <li>Haskell</li>
+                </ul>
+                <ul>
+                    <li>Git</li>
+                    <li>Unix-based OS</li>
+                    <li>3D Printing</li>
+                    <li>Computer Hardware Assembly</li>
+                </ul>
+            </td>
         </tr>
     </table>
 
