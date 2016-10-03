4fa458284a809c55bb9ceb7260e93125cdc462cb
CStanavige <cstanavige12@wou.edu>
2016-10-02 17:04:57 -0700

    Created and styled about page content

--- HW1/about.php
+++ HW1/about.php
@@ -1,5 +1,12 @@
 <?php include ('layouts/header.php'); ?>
 
-<center><h2>Contents of each page will go here</h2></center>
+<center><h1>A Little About Me...</h1></center>
+ <div id="about" class="row">
+     <img src="images/Me.png" alt="Cameron">
+     <p>My name is Cameron Stanavige.</p>
+     <p>I am a full time student at Western Oregon University pursuring a double major in Computer Science and Mathematics. I have a passion for furthering my education and knowledge and am looking for a future career that would allow just that.</p>
+     <p>I enjoy problem solving and having the freedom to try and discover new things.</p>
+     <p>In my spare time I love spending time with my wife and cat, 3D printing, and traveling and discovering new parts of the world.</p>
+ </div>
 
  <?php include ('layouts/footer.php'); ?>
