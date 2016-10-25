6f00e916481f5b6de07ea86cb592b36b5bb63099
CStanavige <cstanavige12@wou.edu>
2016-10-25 00:10:36 -0700

    Finished Loan Calculator page

--- HW4/HW4/Controllers/HomeController.cs
+++ HW4/HW4/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace HW4.Controllers
@@ -48,7 +46,7 @@ namespace HW4.Controllers
         /// GET: PostPage
         /// Gets the PostPage View which contains a form for users to input a username and password.
         /// </summary>
-        /// <returns>The PostPage View Object that represents the result of this action. </returns>
+        /// <returns>The PostPage View Object that represents the result of this action.</returns>
         public ActionResult PostPage()
         {
             return View();
@@ -59,8 +57,8 @@ namespace HW4.Controllers
         /// Take the username and password input from the user, determines their lengths, and performs
         /// a few simple algebra operations on them.
         /// </summary>
-        /// <param name="form"></param>
-        /// <returns></returns>
+        /// <param name="form">The collection of form data from the PostPage View</param>
+        /// <returns>The PostPage View Object that represents the result of this action.</returns>
         [HttpPost]
         public ActionResult PostPage(FormCollection form)
         {
@@ -78,9 +76,63 @@ namespace HW4.Controllers
             return View();
         }
 
+        /// <summary>
+        /// GET: LoanCalc
+        /// Gets the LoanCalc View which contains a form for users to input values
+        /// to determine monthly payments on a loan.
+        /// </summary>
+        /// <returns>The LoanCalc View Object that represents the result of this action.</returns>
         public ActionResult LoanCalc()
         {
             return View();
+        }
+
+        /// <summary>
+        /// POST: LoanCalc
+        /// Takes the three values input by the user and calculates the monthly payment and sum of
+        /// payments and returns that information to the view.
+        /// </summary>
+        /// <param name="principal">The desired amount for the loan.</param>
+        /// <param name="interest">The annual interest rate of the loan.</param>
+        /// <param name="term">The number of years payments will be made.</param>
+        /// <returns>The LoanCalc View Object that represents the result of this action.</returns>
+        [HttpPost]
+        public ActionResult LoanCalc(int? principal, double? interest, int? term)
+        {
+            // Assign the input values to temp variables or give the value of -1 if null
+            int p = principal ?? -1;
+            double i = interest ?? -1.0;
+            int t = term ?? -1;
+            // If any of the values were null, send an error message to the view
+            if ((p == -1) | (i == -1.0) | (t == -1))
+            {
+                ViewBag.Error = "Please enter a value in all fields";
+                return View();
+            }
+
+            // Calculate the monthly payment
+            double monthly = CalculateMonthly(p, i, t);
+
+            // Add the calculated values to the ViewBag
+            ViewBag.Sum = Math.Round(monthly * (t * 12), 2);
+            ViewBag.Monthly = Math.Round(monthly, 2);
+
+            return View();
+        }
+
+        /// <summary>
+        /// Calculate the monthly payment based on the values input by the user.
+        /// </summary>
+        /// <param name="p">The desired amount for the loan.</param>
+        /// <param name="i">The annual interest rate of the loan.</param>
+        /// <param name="t">The number of years payments will be made.</param>
+        /// <returns>The monthly payment.</returns>
+        private double CalculateMonthly(int p, double i, int t)
+        {
+            double monthlyInterest = (i / 100) / 12;
+            double payments = t * 12;
+
+            return p * (monthlyInterest / (1 - Math.Pow((1 + monthlyInterest), -payments)));
         }
     }
 }
