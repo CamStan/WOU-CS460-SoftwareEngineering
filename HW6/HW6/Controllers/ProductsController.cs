afe37ca7b85a0bedac4618c78a58f04707b3960c
CStanavige <cstanavige12@wou.edu>
2016-11-19 01:59:24 -0800

    Implemented paging on the products list page

--- HW6/HW6/Controllers/ProductsController.cs
+++ HW6/HW6/Controllers/ProductsController.cs
@@ -24,17 +24,26 @@ namespace HW6.Controllers
             return View(cat);
         }
 
-        public ActionResult Products(int? id/*, int? page*/)
+        public ActionResult Products(int? id, int? page = 1)
         {
             if (id == null || db.ProductSubcategories.Find(id) == null)
                 return RedirectToAction("Index");
+
             var products = db.ProductSubcategories.Find(id).Products.ToList();
-           // ViewBag.NumberOfProducts = products.Count;
-           // int pageSize = 6;
-            //int pageNumber = page ?? 1;
-           // var productsPaged = products.Skip(pageSize * (pageNumber-1)).Take(pageSize);
-           // look at Bootstrap pagination
-            return View(products);
+
+            int pageSize = 6;
+            double numOfPages = Math.Ceiling((double)products.Count / pageSize);
+
+            int pageNumber = page ?? 0;
+            if (page < 1 || page > numOfPages)
+                return HttpNotFound();
+
+            ViewBag.Page = pageNumber;
+            ViewBag.NumberOfPages = numOfPages;
+
+            var productsPaged = products.Skip(pageSize * (pageNumber - 1)).Take(pageSize);
+
+            return View(productsPaged);
         }
 
         public ActionResult Product(int? id)
@@ -65,7 +74,7 @@ namespace HW6.Controllers
             var image = (thumbnail ?? false) ?
                 db.ProductProductPhotoes.Where(p => p.ProductID == (int)id).FirstOrDefault().ProductPhoto.ThumbNailPhoto :
                 db.ProductProductPhotoes.Where(p => p.ProductID == (int)id).FirstOrDefault().ProductPhoto.LargePhoto;
-            
+
             return File(image, "image/gif");
         }
 
@@ -92,7 +101,7 @@ namespace HW6.Controllers
         [HttpPost]
         public ActionResult Review(ProductReview review)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 db.ProductReviews.Add(review);
                 db.SaveChanges();
