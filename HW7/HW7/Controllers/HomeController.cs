a47c6e77a9e0a5e43d7f7b86aa6f4bd912203544
CStanavige <cstanavige12@wou.edu>
2016-11-22 19:15:34 -0800

    Initial creation and use of AJAX, JSON, and dygraphs

--- HW7/HW7/Controllers/HomeController.cs
+++ HW7/HW7/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,6 +14,23 @@ namespace HW7.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult Stocks(string symbol)
+        {
+            //validate symbol
+            string stockSymbol = symbol;
+            var url = $"http://chart.finance.yahoo.com/table.csv?s={symbol}&a=9&b=22&c=2016&d=10&e=22&f=2016&g=d&ignore=.csv";
+            // web request class
+            WebRequest csvRequest = WebRequest.Create(url);
+            WebResponse csvResponse = csvRequest.GetResponse();
+            StreamReader reader = new StreamReader(csvResponse.GetResponseStream());
+            string file = reader.ReadToEnd();
+            reader.Close();
+            var data = new { csv = file };
+            //var data = new { message = symbol };
+
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ HW7/HW7/Scripts/stocks.js
@@ -0,0 +1,34 @@
+﻿$("#request").click(function () {
+    var symbol = $("#stockSymbol").val();
+    console.log(symbol);
+    var source = "/Home/Stocks/" + symbol;
+    console.log(source);
+    $.ajax({
+        type: "GET",
+        datatype: "json",
+        url: source,
+        success: displayStocks
+    });
+});
+
+function displayStocks(data) {
+    graph = new Dygraph(
+        document.getElementById("graphdiv"),
+        data.csv,
+        {
+            rollPeriod: 7,
+            showRoller: true,
+            Volume : {
+                axis: 'y2'
+            },
+            axis: {
+                y2: {
+                    labelsKMB: true
+                }
+            },
+            //ylabel: 'Primary y-axis',
+            y1label: 'Secondary y-axis'
+        }
+        );
+    //$("#graphdiv").text(data.csv);
+}
