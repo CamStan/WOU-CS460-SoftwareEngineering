0b34a2f9286fb4aac4387b10f7544d17b561aa90
CStanavige <cstanavige12@wou.edu>
2016-11-29 23:51:36 -0800

    Created and implemented Booty page to display each pirates booty using ajax and json

--- HW8/HW8/Controllers/HomeController.cs
+++ HW8/HW8/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HW8.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,35 @@ namespace HW8.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Booty()
+        {
+            return View();
+        }
+
+        public JsonResult GetBooty()
+        {
+            string[] pirateBooty = null;
+
+            using (PirateUnionContext db = new PirateUnionContext())
+            {
+                var pb = db.Crews.GroupBy(c => c.PirateID).Select(p => new { Pirate = p.Key, TotalBooty = p.Sum(b => b.Booty) }).OrderByDescending(bp => bp.TotalBooty).ToList();
+
+                pirateBooty = new string[pb.Count];
+
+                for (int i = 0; i < pirateBooty.Length; ++i)
+                {
+                    pirateBooty[i] = $"<tr><td>{db.Pirates.Find(pb[i].Pirate).FullName}</td><td>${pb[i].TotalBooty}</td></tr>";
+                }
+            }
+
+            var data = new
+            {
+                arr = pirateBooty
+            };
+
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ HW8/HW8/Scripts/booty.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    var source = "/Home/GetBooty";
+    $.ajax({
+        type: "GET",
+        datatype: "json",
+        url: source,
+        success: displayBooty
+    });
+});
+
+function displayBooty(data) {
+    var table = $("#bootyTable"),
+        thead = $('<tr><th>Pirate</th><th>Total Booty</th></tr>');
+    thead.appendTo(table);
+
+    data.arr.forEach(function (pb) {
+        table.append(pb);
+    });
+}
