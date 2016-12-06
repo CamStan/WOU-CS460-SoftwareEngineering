741698e27df4c8a13115974d75f4e788797eac57
CStanavige <cstanavige12@wou.edu>
2016-12-06 14:49:20 -0800

    Created sql script files to create and populate database. Built models from the database

---
+++ Final/Final/App_Data/initDB.sql
@@ -0,0 +1,67 @@
+﻿-- *********** Drop Tables ***********
+-- DONT RUN THIS SECTION WHEN RUNNING SCRIPT THE FIRST TIME
+ALTER TABLE [dbo].[ArtWorks]
+	DROP CONSTRAINT [FK_dbo.Artworks_dbo.ArtistID];
+
+ALTER TABLE [dbo].[Classifications]
+	DROP CONSTRAINT [FK_dbo.Classifications_dbo.ArtWorkID];
+
+ALTER TABLE [dbo].[Classifications]
+DROP CONSTRAINT [FK_dbo.Classifications_dbo.GenreID];
+
+IF OBJECT_ID('dbo.Artists','U') IS NOT NULL
+	DROP TABLE [dbo].[Artists];
+GO
+
+IF OBJECT_ID('dbo.Genres','U') IS NOT NULL
+	DROP TABLE [dbo].[Genres];
+GO
+
+IF OBJECT_ID('dbo.ArtWorks','U') IS NOT NULL
+	DROP TABLE [dbo].[ArtWorks];
+GO
+
+IF OBJECT_ID('dbo.Classifications','U') IS NOT NULL
+	DROP TABLE [dbo].[Classifications];
+GO
+
+-- ########### Artists ###########
+CREATE TABLE [dbo].[Artists]
+(
+	[ID] INT IDENTITY (1,1) NOT NULL,
+	[FirstName] NVARCHAR (50) NOT NULL,
+	[LastName] NVARCHAR (50) NOT NULL,
+	[BirthDate] DATETIME2 NOT NULL,	
+	[BirthCity] NVARCHAR (50) NOT NULL,
+	[BirthCountry] NVARCHAR (50) NOT NULL,
+	CONSTRAINT [PK_dbo.Artists] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+-- ########### Genres ###########
+CREATE TABLE [dbo].[Genres]
+(
+	[ID] INT IDENTITY (1,1) NOT NULL,
+	[Name] NVARCHAR (50) NOT NULL,
+	CONSTRAINT [PK_dbo.Genres] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+-- ########### ArtWorks ###########
+CREATE TABLE [dbo].[ArtWorks]
+(
+	[ID] INT IDENTITY (1,1) NOT NULL,	
+	[Title] NVARCHAR (100) NOT NULL,
+	[ArtistID] INT NOT NULL,
+	CONSTRAINT [PK_dbo.Artworks] PRIMARY KEY CLUSTERED ([ID] ASC),
+	CONSTRAINT [FK_dbo.Artworks_dbo.ArtistID] FOREIGN KEY ([ArtistID]) REFERENCES [dbo].[Artists] ([ID])
+);
+
+-- ########### Classifications ###########
+CREATE TABLE [dbo].[Classifications]
+(
+	[ID] INT IDENTITY (1,1) NOT NULL,
+	[ArtWorkID] INT NOT NULL,
+	[GenreID] INT NOT NULL,
+	CONSTRAINT [PK_dbo.Classifications] PRIMARY KEY CLUSTERED ([ID] ASC),
+	CONSTRAINT [FK_dbo.Classifications_dbo.ArtWorkID] FOREIGN KEY ([ArtWorkID]) REFERENCES [dbo].[ArtWorks] ([ID]),
+	CONSTRAINT [FK_dbo.Classifications_dbo.GenreID] FOREIGN KEY ([GenreID]) REFERENCES [dbo].[Genres] ([ID])
+);
---
+++ Final/Final/App_Data/populate.sql
@@ -0,0 +1,29 @@
+﻿-- ########### Artists ###########
+INSERT INTO [dbo].[Artists](FirstName,LastName,BirthDate,BirthCity,BirthCountry)VALUES('M.C.','Escher','06/17/1898','Leeuwarden','Netherlands');
+INSERT INTO [dbo].[Artists](FirstName,LastName,BirthDate,BirthCity,BirthCountry)VALUES('Leonardo','Da Vinci','05/02/1519','Vinci','Italy');
+INSERT INTO [dbo].[Artists](FirstName,LastName,BirthDate,BirthCity,BirthCountry)VALUES('Hatip Mehmed','Efendi','11/19/1680','Unknown','Unknown');
+INSERT INTO [dbo].[Artists](FirstName,LastName,BirthDate,BirthCity,BirthCountry)VALUES('Salvador','Dali','05/11/1904','Figueres','Spain');
+
+-- ########### Genres ###########
+INSERT INTO [dbo].[Genres](Name)VALUES('Tesselation');
+INSERT INTO [dbo].[Genres](Name)VALUES('Surrealism');
+INSERT INTO [dbo].[Genres](Name)VALUES('Portrait');
+INSERT INTO [dbo].[Genres](Name)VALUES('Renaissance');
+
+-- ########### ArtWorks ###########
+INSERT INTO [dbo].[ArtWorks](Title,ArtistID)VALUES('Circle Limit III',1);
+INSERT INTO [dbo].[ArtWorks](Title,ArtistID)VALUES('Twon Tree',1);
+INSERT INTO [dbo].[ArtWorks](Title,ArtistID)VALUES('Mona Lisa',2);
+INSERT INTO [dbo].[ArtWorks](Title,ArtistID)VALUES('The Vitruvian Man',2);
+INSERT INTO [dbo].[ArtWorks](Title,ArtistID)VALUES('Ebru',3);
+INSERT INTO [dbo].[ArtWorks](Title,ArtistID)VALUES('Honey Is Sweeter Than Blood',4);
+
+-- ########### Classifications ###########
+INSERT INTO [dbo].[Classifications](ArtWorkID,GenreID)VALUES(1,1);
+INSERT INTO [dbo].[Classifications](ArtWorkID,GenreID)VALUES(2,1);
+INSERT INTO [dbo].[Classifications](ArtWorkID,GenreID)VALUES(2,2);
+INSERT INTO [dbo].[Classifications](ArtWorkID,GenreID)VALUES(3,3);
+INSERT INTO [dbo].[Classifications](ArtWorkID,GenreID)VALUES(3,4);
+INSERT INTO [dbo].[Classifications](ArtWorkID,GenreID)VALUES(4,4);
+INSERT INTO [dbo].[Classifications](ArtWorkID,GenreID)VALUES(5,1);
+INSERT INTO [dbo].[Classifications](ArtWorkID,GenreID)VALUES(6,2);
