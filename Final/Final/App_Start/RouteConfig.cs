4da17e0295dceebe7774716e96201fb0b2dd9be6
CStanavige <cstanavige12@wou.edu>
2016-12-06 18:01:59 -0800

    AJAX call to list artwork by genre

--- Final/Final/App_Start/RouteConfig.cs
+++ Final/Final/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Final
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Genre",
+                url: "Home/GetGenre/{genre}",
+                defaults: new { controller = "Home", action = "GetGenre"}
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- Final/Final/Controllers/HomeController.cs
+++ Final/Final/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Final.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,31 @@ namespace Final.Controllers
 {
     public class HomeController : Controller
     {
+        private ArtContext db = new ArtContext();
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(db.Genres.OrderBy(g => g.Name).ToList());
+        }
+
+        public JsonResult GetGenre(int genre)
+        {
+            var pas = db.Genres.Find(genre).Classifications.ToList().Select(a => new { Piece = a.ArtWorkID, Art = a.ArtWork.ArtistID }).ToList();
+
+            string[] pieceArtist = new string[pas.Count()];
+
+            for (int i = 0; i < pieceArtist.Length; ++i)
+            {
+                pieceArtist[i] = $"<tr><td>{db.ArtWorks.Find(pas[i].Piece).Title}</td><td>{db.Artists.Find(pas[i].Art).FullName}</td></tr>";
+            }
+
+            var data = new
+            {
+                arr = pieceArtist
+            };
+
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
